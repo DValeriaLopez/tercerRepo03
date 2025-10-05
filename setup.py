0009d0e38bc1f5bcbb9555b5284dc5128034ed7a
Valeria Lopez <DValeriaLopez@users.noreply.github>
2025-10-04 21:51:31 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tercerRepo03",                                    # Nombre del paquete
+    version="0.1.0",                                        # Versión inicial
+    packages=find_packages(),                               # Paquetes a incluir
+    description="Un paquete pip simple de saludo",          # Breve descripción
+    author="Valeria Lopez",                                 # Tu nombre
+    author_email="DValeriaLopez@users.noreply.github",      # Tu correo electrónico
+    url="https://github.com/DValeriaLopez/tercerRepo03",    # URL del proyecto
+)
